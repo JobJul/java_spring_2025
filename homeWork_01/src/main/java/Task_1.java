0a7ce098fe937857650bda3e6db8575945b669fc
JullMann <julia.mannanova@gmail.com>
2025-03-07 02:28:57 +0300

    Выполнена домашка hw_01

---
+++ homeWork_01/src/main/java/Task_1.java
@@ -0,0 +1,8 @@
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("3\n" +
+                "2\n" +
+                "1\n" +
+                "Поехали!!!");
+    }
+}
---
+++ homeWork_01/src/main/java/Task_1_3.java
@@ -0,0 +1,12 @@
+public class Task_1_3 {
+    public static void main(String[] args) {
+
+        // инфа со стр. https://en.wikipedia.org/wiki/Code_page_437
+        // https://en.m.wikipedia.org/wiki/Miscellaneous_Symbols
+        System.out.println("\ud83d\ude00");
+        System.out.println("\u263a");
+        System.out.println("\u266b");
+        System.out.println("\u2649");
+        System.out.println("\u26c4");
+    }
+}
